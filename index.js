441676d90e8b02f1a9f8737feec3aa409c5af6b3
Yeasir Ahamed <yahamed007@gmail.com>
2025-05-31 02:16:46 +0600

    add _curry3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import add from './source/add.js';
-console.log(add(4)(6));
+import _ from './source/_.js';
+import _curry3 from './source/internal/_curry3.js';
+const { log } = console;
+
+const countTriple = _curry3(function countTriple(a, b, c) {
+  return add(c, add(a, b));
+});
+
+log(countTriple(_)(6)(7, 10));
+log(add(_)(6)(7));
